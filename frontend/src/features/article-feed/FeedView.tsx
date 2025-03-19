da1b21c1f73ed4c07d1af7b89cb09607e982f754
Colin B. <vogon@outlook.com>
2025-03-18 22:43:09 -0700

    handle rich text in article bodies, handle HTML entities in article titles, display content and categories fields

--- frontend/src/features/article-feed/FeedView.tsx
+++ frontend/src/features/article-feed/FeedView.tsx
@@ -2,8 +2,10 @@ import { useAppSelector } from "../../app/hooks";
 import { selectFeedListState } from "../feed-list/feed-list-slice";
 import { selectArticles } from "./article-feed-slice";
 import { AbsoluteTimestamp } from "../../components/AbsoluteTimestamp";
-import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { Cog6ToothIcon, GlobeAltIcon, MinusCircleIcon } from "@heroicons/react/24/outline";
 import { BrowserOpenURL } from "../../../wailsjs/runtime/runtime";
+import DOMPurify from "dompurify";
+import { Button } from "@headlessui/react";
 
 export function FeedView() {
     const feedListState = useAppSelector(selectFeedListState);
@@ -11,19 +13,44 @@ export function FeedView() {
 
     const activeFeed = feedListState.activeFeed ? feedListState.feeds[feedListState.activeFeed] : null;
 
+    // create a spare element to use for decoding HTML entities in article titles
+    const entityDecodingScratchpad = document.createElement("div");
+
     return <div className="p-4">
-        {activeFeed ?
-            <>
-                <h1 className="text-2xl font-bold">{activeFeed.FeedTitle}</h1> 
-                <h2 className="text-lg italic">updated at <AbsoluteTimestamp timestampISO={activeFeed.LastUpdatedISO} /></h2>
-            </>             
+        <header className="flex flex-row">
+            {activeFeed ?
+                <>
+                    <div>
+                        <h1 className="text-2xl font-bold">{activeFeed.FeedTitle}</h1> 
+                        <h2 className="text-lg italic">updated at <AbsoluteTimestamp timestampISO={activeFeed.LastUpdatedISO} /></h2>
+                    </div>
+                    <div className="flex flex-row flex-1 gap-4 justify-end items-center">
+                        <Button className="btn btn-secondary">
+                            <Cog6ToothIcon width="24" />
+                            feed settings
+                        </Button>
+                        <Button className="btn btn-warning">
+                            <MinusCircleIcon width="24" />
+                            unsubscribe
+                        </Button>
+                    </div>
+                </>
             : null}
+        </header>
 
         {
             articles.map((article) => {
-                return <article key={article.GUID} className="border-2 rounded-box my-4 p-4">
+                // decode HTML entities in article title
+                const purifiedTitle = DOMPurify.sanitize(article.Title);
+                entityDecodingScratchpad.innerHTML = purifiedTitle;
+                const purifiedTitleText = entityDecodingScratchpad.textContent;
+
+                const purifiedDescription = DOMPurify.sanitize(article.Description);
+                const purifiedContent = DOMPurify.sanitize(article.Content);
+
+                return <article key={article.GUID} className="border-2 rounded-box my-4 p-4 max-h-60 overflow-hidden">
                     <span className="text-2xl font-bold">
-                        {article.Title}&nbsp;
+                        {purifiedTitleText !== "" ? purifiedTitleText : <span className="opacity-50">(no title)</span>}&nbsp;
                         
                         <a 
                             className="inline-block text-primary cursor-pointer" 
@@ -33,7 +60,14 @@ export function FeedView() {
                         </a>
                     </span>
                     <p className="italic">published at <AbsoluteTimestamp timestampISO={article.PubDateISO} /></p>
-                    <p className="line-clamp-4">{article.Description}</p>
+                    <p className="line-clamp-4" dangerouslySetInnerHTML={{__html: purifiedDescription}} />
+                    <p className="line-clamp-4" dangerouslySetInnerHTML={{__html: purifiedContent}} />
+                    <p className="italic">
+                        {article.Categories ? 
+                            `(categories: ${article.Categories.join(", ")})`
+                            : null
+                        }
+                    </p>
                 </article>
             })
         }
